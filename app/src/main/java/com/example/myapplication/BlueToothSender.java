172ec1e5ec8ee0aa9184f919a008c293ffb4b1ad
ferder1ke <22s121106@stu.hit.edu.cn>
2024-02-28 09:25:01 +0800

    First commit for Electronic Chemistry WorkStation

---
+++ app/src/main/java/com/example/myapplication/BlueToothSender.java
@@ -0,0 +1,380 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+//scrcpy
+public class
+BlueToothSender extends AppCompatActivity {
+    private EditText InitE, FinalE , Scan_R, Sweep_s, sampling_I, Increse_E;
+    private Switch InitPN;
+
+    private EditText Amplitude, Pulse_P, Sampling, Ebrich_T, HighE, LowE, Pulse_W;
+//    private EditText InitE = findViewById(R.id.InitE), FinalE = findViewById(R.id.FinalE),
+//            Scan_R = findViewById(R.id.Scan_R), Sweep_s = findViewById(R.id.Sweep_s),
+//            sampling_I = findViewById(R.id.sampling_I), Increse_E = findViewById(R.id.Increse_E),
+//            Enrich_E = findViewById(R.id.Enrich_E), Amplitude = findViewById(R.id.Amplitude),
+//            Pulse_P = findViewById(R.id.Pulse_P), Sampling = findViewById(R.id.Sampling),
+//            Ebrich_T = findViewById(R.id.Ebrich_T);
+    private BluetoothSocket mmSocket;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private double lowerLimit = -10.0;
+    private double upperLimit = 10.0;
+    private double stepSize = 0.1;
+
+    private Map<String, Byte> optionByteMap = new HashMap<>();
+    private byte[] Pkg = new byte[23];
+//    private static byte[] convertArrayListToByteArray(ArrayList<Integer> intArrayList) {
+//        ByteBuffer byteBuffer = ByteBuffer.allocate(intArrayList.size());
+//        for (Integer intValue : intArrayList) {
+//            byteBuffer.putInt(intValue);
+//        }
+//        return byteBuffer.array();
+//    }
+
+    private byte[] setUsignHalfWord2PointFoward(EditText editText) { //小数点后两位
+        String curEditTextString = editText.getText().toString();
+        double curEditTextVal = Double.parseDouble(curEditTextString);
+        if(curEditTextVal > 655.35) {
+            editText.setText("655.35");
+            curEditTextVal = 655.35;
+        }
+        else if(curEditTextVal < 0){
+            editText.setText("0");
+            curEditTextVal = 0;
+        }
+        curEditTextVal *= 100;
+        byte[] res = new byte[2];
+        int temp = (int) curEditTextVal;
+        res[0] = (byte) ((temp >> 8) & 0xff);
+        res[1] = (byte) (temp & 0xff);
+        return res;
+    }
+
+    private byte[] setSignHalfWord(EditText editText) { //保留整数
+        String curEditTextString = editText.getText().toString();
+        int curEditTextVal = Integer.parseInt(curEditTextString);
+        if(curEditTextVal > 32766){
+            editText.setText("32766");
+            curEditTextVal = 32766;
+        }
+        else if(curEditTextVal < -32766){
+            editText.setText("-32766");
+            curEditTextVal = -32766;
+        }
+        byte[] res = new byte[2];
+        if(curEditTextVal < 0) {
+            res[0] |= 0x80;
+            curEditTextVal *= -1;
+        }
+        res[0] |= (byte) ((curEditTextVal >> 8) & 0xff);
+        res[1] = (byte) (curEditTextVal & 0xff);
+        return res;
+    }
+    private byte[] setUsignHalfWord1PointFoward(EditText editText) { //小数点后一位
+        String curEditTextString = editText.getText().toString();
+        double curEditTextVal = Double.parseDouble(curEditTextString);
+        if(curEditTextVal > 6553.5) {
+            editText.setText("6553.5");
+            curEditTextVal = 6553.5;
+        }
+        else if(curEditTextVal < 0){
+            editText.setText("0");
+            curEditTextVal = 0;
+        }
+        curEditTextVal *= 10;
+        byte[] res = new byte[2];
+        int temp = (int) curEditTextVal;
+        res[0] = (byte) ((temp >> 8) & 0xff);//高位
+        res[1] = (byte) (temp & 0xff);//低位
+        return res;
+    }
+
+    private byte[] setUsign5Word(EditText editText) {//保留整数
+        String curEditTextString = editText.getText().toString();
+        int curEditTextVal = Integer.parseInt(curEditTextString);
+        if(curEditTextVal > 655350) {
+            editText.setText("655350");
+            curEditTextVal = 655350;
+        }
+        else if(curEditTextVal < 0){
+            editText.setText("0");
+            curEditTextVal = 0;
+        }
+        curEditTextVal /= 10;
+        byte[] res = new byte[2];
+        res[0] = (byte) ((curEditTextVal >> 8) & 0xff);//高位
+        res[1] = (byte) (curEditTextVal & 0xff);//低位
+        return res;
+    }
+
+    private byte[] setUsignHalfWord(EditText editText) {
+        String curEditTextString = editText.getText().toString();
+        double curEditTextVal = Double.parseDouble(curEditTextString);
+        if(curEditTextVal > 65535){
+            editText.setText("65535");
+            curEditTextVal = 65535;
+        }
+        else if(curEditTextVal < 0){
+            editText.setText("0");
+            curEditTextVal = 0;
+        }
+        byte[] res = new byte[2];
+        int temp = (int) curEditTextVal;
+        res[0] = (byte) ((temp >> 8) & 0xff);
+        res[1] = (byte) (temp & 0xff);
+        return res;
+    }
+
+    private byte setsignedByte(EditText editText){//检查带符号数据
+        String curEditTextString = editText.getText().toString();
+        double curEditTextVal = Double.parseDouble(curEditTextString);
+        if(curEditTextVal > 12.7)
+            editText.setText("12.7");
+        else if(curEditTextVal < -12.7){
+            editText.setText("-12.7");
+        }
+
+        String interStringTmp;
+        int interInterTmp;
+        interStringTmp = editText.getText().toString();
+        interInterTmp = (int) (Double.parseDouble(interStringTmp) * 10);
+        if(interInterTmp < 0){
+            interInterTmp *= -1;
+            byte temp = (byte) interInterTmp;
+            temp |= (byte) 0x80;
+            return temp;
+        }
+        return (byte) interInterTmp;
+    }
+    private byte setUnsignedByte(EditText editText){
+        String curEditTextString = editText.getText().toString();
+        int curEditTextVal = Integer.parseInt(curEditTextString);
+        if(curEditTextVal > 255)
+            editText.setText("255");
+
+        String interStringTmp;
+        int interInterTmp;
+        interStringTmp = editText.getText().toString();
+        interInterTmp = Integer.parseInt(interStringTmp);
+        return (byte) interInterTmp;
+    }
+//    @android.support.annotation.RequiresApi(api = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @SuppressLint("MissingPermission")
+    private void sendPkg(BluetoothSocket socket, byte[] pkg){
+        try {
+            if (socket != null){
+                if(!socket.isConnected()){
+                    mmSocket.connect();
+                }
+                OutputStream outputStream = socket.getOutputStream();
+                if(outputStream != null){
+                    outputStream.write(pkg);
+                    outputStream.flush();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private static class RangeInputFilter implements InputFilter {
+        private double minValue;
+        private double maxValue;
+        private double stepSize;
+
+        public RangeInputFilter(double minValue, double maxValue, double stepSize) {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.stepSize = stepSize;
+        }
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end,
+                                   Spanned dest, int dstart, int dend) {
+            try {
+                // 将输入字符连接到原始文本
+                String newVal = dest.toString().substring(0, dstart) + source.toString() + dest.toString().substring(dend);
+
+                // 尝试将新值转换为double
+                double inputVal = Double.parseDouble(newVal);
+
+                // 检查是否在范围内，如果是，返回null表示接受输入
+                if (isInRange(inputVal)) {
+                    return null;
+                }
+            } catch (NumberFormatException e) {
+                // 当输入为空或无法转换为double时，忽略异常
+            }
+
+            // 输入不在范围内，返回空表示不接受输入
+            return "";
+        }
+
+        private boolean isInRange(double value) {
+            // 检查是否在指定的范围内，并且是步长的倍数
+            return value >= minValue && value <= maxValue && (value - minValue) % stepSize == 0;
+        }
+    }
+    private void handleTextChange(Editable editable, EditText editText) {
+        if (editable.length() > 0) {
+            double currentValue = Double.parseDouble(editable.toString());
+
+            // 验证输入值是否在范围内，并按步长调整
+            if (currentValue < lowerLimit) {
+                currentValue = lowerLimit;
+            } else if (currentValue > upperLimit) {
+                currentValue = upperLimit;
+            } else {
+                currentValue = Math.round(currentValue / stepSize) * stepSize;
+            }
+
+            // 更新EditText的文本
+            editText.setText(String.valueOf(currentValue));
+            // 将光标移到文本末尾
+            editText.setSelection(editText.getText().length());
+        }
+    }
+    @Override
+    @SuppressLint("MissingPermission")
+     protected void onCreate(@Nullable Bundle savedInstanceState){
+         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+         //界面初始化
+         setContentView(R.layout.activity_send_pkg);
+         InitE = findViewById(R.id.InitE);
+         FinalE = findViewById(R.id.FinalE);
+         Scan_R = findViewById(R.id.Scan_R);
+         Sweep_s = findViewById(R.id.Sweep_s);
+         sampling_I = findViewById(R.id.sampling_I);
+         Increse_E = findViewById(R.id.Increse_E);
+
+         InitPN = findViewById(R.id.InitPN);
+         Amplitude = findViewById(R.id.Amplitude);
+         Pulse_P = findViewById(R.id.Pulse_P);
+         Sampling = findViewById(R.id.Sampling);
+         Ebrich_T = findViewById(R.id.Ebrich_T);
+
+         HighE = findViewById(R.id.HighE);
+         LowE = findViewById(R.id.LowE);
+         Pulse_W = findViewById(R.id.Pulse_W);
+         Button mButton = findViewById(R.id.button2);
+         Spinner spinner = findViewById(R.id.CMD);
+
+         List<String> options = new ArrayList<>();
+         options.add("循环伏安法(CV_Rest)");
+         options.add("方波脉冲伏安法(CV_Rest)");
+         options.add("差分脉冲伏安法(DPV_Rest)");
+         options.add("数据流(电压电流)");
+        optionByteMap.put("循环伏安法(CV_Rest)", (byte) 0x71);
+        optionByteMap.put("方波脉冲伏安法(CV_Rest)", (byte) 0x72);
+        optionByteMap.put("差分脉冲伏安法(DPV_Rest)", (byte) 0x73);
+        optionByteMap.put("数据流(电压电流)", (byte) 0x81);
+
+         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, options);
+         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+         spinner.setAdapter(adapter);
+         Intent intent = getIntent();
+         //InitE.setFilters(new InputFilter[]{new RangeInputFilter(-10, 10.0, 0.1)});
+         //构建mmSocket通道
+         String deviceAddress = intent.getStringExtra("device_address");
+        BluetoothDevice device = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(deviceAddress);
+        try {
+            mmSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+            mmSocket.connect();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+         mButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 try {
+                     //int num = Integer.parseInt(number);
+//                     Intent intent = getIntent();
+//                     String deviceAddress = intent.getStringExtra("device_address");
+//                     BluetoothDevice device = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(deviceAddress);
+                     Pkg[0] = (byte) 0x5A;
+                     Pkg[1] = (byte) 0xA5;
+                     //判断控制流
+                     String controlFlow = (String) spinner.getSelectedItem();
+                     byte controlByte = optionByteMap.get(controlFlow);
+                     Pkg[2] = controlByte;           //CMD
+                     Pkg[3] = setsignedByte(InitE);  //InitE
+                     Pkg[4] = setsignedByte(FinalE); //FinalE
+
+                     byte[] temp;
+                     temp = setUsignHalfWord2PointFoward(Scan_R);//Scan_R u16
+                     Pkg[5] = temp[0];
+                     Pkg[6] = temp[1];
+                     //Pkg[5] = setUnsignedByte(Scan_R);//u16
+                     //Pkg[6] = setUnsignedByte(Scan_R);//Scan_R
+
+                     Pkg[7] = setUnsignedByte(Sweep_s);//Sweeps
+                     Pkg[8] = setUnsignedByte(sampling_I);//sampling_I
+                     Pkg[9] = setUnsignedByte(Increse_E);//Increse_E
+
+                     if (InitPN.isChecked())   //InitPN
+                        Pkg[10] = 0X00;
+                     else
+                         Pkg[10] = 0x01;
+                     temp = setSignHalfWord(Amplitude);//Ampltitude signed16
+                     Pkg[11] = temp[0];
+                     Pkg[12] = temp[1];
+                     //Pkg[11] = setUnsignedByte(Amplitude);//u16
+                     //Pkg[12] = setUnsignedByte(Amplitude);//Ampltitude
+
+                     temp = setUsign5Word(Pulse_P);//Pulse_P u16
+                     Pkg[13] = temp[0];
+                     Pkg[14] = temp[1];
+                     //Pkg[13] = setUnsignedByte(Pulse_P);//u16
+                     //Pkg[14] = setUnsignedByte(Pulse_P);//Pulse_P
+
+                     temp = setUsignHalfWord1PointFoward(Sampling);
+                     Pkg[15] = temp[0];
+                     Pkg[16] = temp[1];
+                     //Pkg[15] = setUnsignedByte(Sampling);//u16
+                     //Pkg[16] = setUnsignedByte(Sampling);//Sampling
+
+                     Pkg[17] = setUnsignedByte(Ebrich_T);//Ebrich_T
+                     Pkg[18] = setsignedByte(HighE);//HighE
+                     Pkg[19] = setsignedByte(LowE);//LowE
+
+                     temp = setUsignHalfWord(Pulse_W);
+                     Pkg[20] = temp[0];
+                     Pkg[21] = temp[1];
+                     //Pkg[20] = setUnsignedByte(Pulse_W);//u16
+                     //Pkg[21]  = setUnsignedByte(Pulse_W);//Pulse
+
+                   //  Pkg[22] = setUnsignedByte(CRC);
+                     sendPkg(mmSocket, Pkg);
+                     Toast.makeText(BlueToothSender.this, "发送成功", Toast.LENGTH_SHORT).show();
+                 } catch (NumberFormatException e){
+                     Toast.makeText(BlueToothSender.this, "输入的不是有效", Toast.LENGTH_SHORT).show();
+                 }
+             }
+         });
+     }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,193 @@
+package com.example.myapplication;// MainActivity.java
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    public  BluetoothSocket mmSocket;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter bluetoothAdapter;
+    private EditText dataDisplay;
+
+    private  LineChart lineChart;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // Standard SerialPortService ID
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        Button connectButton = findViewById(R.id.connectButton);
+        dataDisplay = findViewById(R.id.dataDisplay);
+        Log.d("MyApp", "onCreate executed");
+
+        lineChart = findViewById(R.id.lineChart);
+        setData();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "设备不支持蓝牙", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            @SuppressLint("MissingPermission")
+            public void onClick(View v) {
+                if (!bluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                } else {
+                    selectBluetoothDevice();
+                }
+            }
+        });
+    }
+
+    public void setData(){
+        float datas[] = {14f,15f,16f,17f,16f,16f};
+        //在MPAndroidChart一般都是通过List<Entry>对象来装数据的
+        List<Entry> entries = new ArrayList<Entry>();
+        //循环取出数据
+        for(int i = 0; i < datas.length; i++){
+            entries.add(new Entry(i,datas[i]));
+        }
+        //一个LineDataSet对象就是一条曲线
+        LineDataSet lineDataSet = new LineDataSet(entries,"第一条数据");
+        //LineData才是正真给LineChart的数据
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+    }
+
+
+    @Override
+    @SuppressLint("MissingPermission")
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                selectBluetoothDevice();
+            } else {
+                Toast.makeText(this, "为启动蓝牙", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == 2 && resultCode == RESULT_OK) {
+            String address = data.getStringExtra("address");
+            BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+            try {
+                mmSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+                mmSocket.connect();
+                startListening(mmSocket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void startListening(BluetoothSocket socket) {
+        try {
+            Toast.makeText(this, "蓝牙链接成功", Toast.LENGTH_SHORT).show();
+            InputStream mmInputStream = socket.getInputStream();
+            BufferedReader mmBufferedReader = new BufferedReader(new InputStreamReader(mmInputStream));
+            Thread listenThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    byte[] buffer = new byte[1024];
+                    int bytes;
+                    InputStream is = null;
+                    try {
+                        is = socket.getInputStream();
+                        while (!Thread.currentThread().isInterrupted()) {
+                            while((bytes = is.read(buffer)) > 0){
+                                byte[] buf_data = new byte[bytes];
+                                for(int i = 0; i < bytes; i++){
+                                    buf_data[i] = buffer[i];
+                                }
+                                String tmp = "";
+                                for (int i = 0; i < buf_data.length; i++) {
+                                    int v = buf_data[i]& 0xFF;
+                                    String s = Integer.toHexString(v);
+                                    tmp += s;
+                                }
+                                dataDisplay.append(tmp + "\n");
+                            }
+                            String receivedData = mmBufferedReader.readLine();
+                            if (receivedData != null) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        dataDisplay.append(receivedData + "\n");
+                                    }
+                                });
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            listenThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    private void selectBluetoothDevice() {
+        Intent intent = new Intent(this, DeviceListActivity.class);
+        startActivityForResult(intent, 2);
+    }
+
+    //蓝牙搜寻模式 这里就是mainActivity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.pkg_send){
+            Intent intent = new Intent(this, BlueToothSender.class);
+            if(mmSocket != null){
+                String deviceAddress = this.mmSocket.getRemoteDevice().getAddress();
+                intent.putExtra("device_address", deviceAddress);
+            }
+            startActivity(intent);
+            return true;
+        }else if(item.getItemId() == R.id.bluetooth_search){
+            selectBluetoothDevice();
+            return true;
+        }
+        return true;
+    }
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
